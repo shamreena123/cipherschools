3d0541ea48e3741f05096b45baa6a8f04d4376f2
shamreena123 <104267135+shamreena123@users.noreply.github.com>
2024-02-04 17:40:27 +0530

    Add files via upload

---
+++ w2p3/javascript.js
@@ -0,0 +1,56 @@
+//console.log("My console statement")
+
+//console.log(10);
+
+
+
+
+
+/*var score=10;
+let result=50;
+const pi=3.14;
+
+score=15;
+result=55;
+pi=3.26;
+
+console.log(score);
+console.log(result);
+console.log(pi);*/
+
+
+
+
+
+//Datatypes
+
+/*There are 7 types of datatypes in javascript.
+Number,Strings,Boolean,Null,Undefined, Objects,Symbols */
+
+//BEDMAS (bracket exponent division multiplication addition subtraction)
+let score=10; //number
+let result='10';  //string
+let finalScore = score+result;
+let penalty= score-result;
+/*let totalGames=5;
+let avgScore=finalScore/totalGames;
+
+console.log(score,result);
+console.log(finalScore);
+console.log(penalty);
+console.log(avgScore);
+
+//typeof
+console.log(typeof finalScore);
+console.log(typeof penalty);
+console.log(typeof avgScore);
+
+
+//BEDMAS (bracket exponent division multiplication addition subtraction)
+/*let value=(finalScore*2)*10/(penalty+totalGames);
+console.log(value);*/
+
+let totalGamess='five';
+let value=(finalScore*2)*10/(penalty+totalGamess);
+console.log(value);
+console.log(typeof value);
