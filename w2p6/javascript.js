aa45be9e37691a8b6752cc41fcf998c062be9856
shamreena123 <104267135+shamreena123@users.noreply.github.com>
2024-02-04 19:34:01 +0530

    Add files via upload

---
+++ w2p6/javascript.js
@@ -0,0 +1,129 @@
+//var abc=10;
+
+/*function testing(){
+    var abc=20;
+    let xyz='block scoped';
+    console.log(abc);
+
+    if(1){
+        console.log(xyz);
+        console.log(abc);
+    }
+}
+
+testing();*/
+
+
+
+
+
+/*function testing(){
+    var abc=20;
+    console.log(abc);
+
+    if(1){
+        let xyz='block scope';
+        console.log('Inside if',xyz);
+        console.log(abc);
+    }
+    console.log('Outside if', xyz);
+}
+
+testing();*/
+
+
+
+
+
+/*function testing(){
+    var abc=20;
+    console.log(abc);
+
+    if(1){
+        var xyz='block scoped';
+        console.log('Insideif',xyz);
+        console.log(abc);
+    }
+    console.log('Outside if', xyz);
+}
+
+testing();
+
+
+
+
+
+
+
+var namee='Cipher';
+let year=2020;
+function test(){
+    console.log(namee);
+    console.log(year);
+}
+
+test();*/
+
+
+
+
+
+
+/*myFunc();
+myFunc2();//hoisting problem
+
+function myFunc(){
+    console.log('Inside myFunc declaration');
+}
+
+
+let myFunc2=function(){
+    console.log('Inside myFunc2')
+}*/
+
+/*console.log(namee);
+console.log(name2);
+
+var namee='Apoorva';
+let name='Mridula'*/
+
+
+
+
+
+
+/*let newFunc =()=> {
+    console.log('Arrow Function');
+    console.log(this);
+}
+
+newFunc();
+
+
+
+let funcExp = function(){
+    console.log('Function exp created');
+    console.log(this);
+}
+
+funcExp*/
+
+
+
+
+
+
+/*let newFunc= (namee)=>{
+    console.log('Arrow');
+    console.log(namee);
+}
+
+newFunc('cipher');*/
+
+
+
+
+
+let newFunc= (namee)=>{console.log(namee);}
+
+newFunc('cipher');
