a8f3538b19c4d7a981b89ce3450dd40cd46e660c
shamreena123 <104267135+shamreena123@users.noreply.github.com>
2024-02-04 22:52:06 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App(props) {
+  return (
+    <div className='pizza'>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ Dominoes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dominoes(props) {
+  return (
+    <div className='pizza'>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+export default Dominoes
