183d733f9f111657405638946c965dd970f726a8
shamreena123 <104267135+shamreena123@users.noreply.github.com>
2024-02-04 18:18:43 +0530

    Add files via upload

---
+++ w2p4/javascript.js
@@ -0,0 +1,53 @@
+console.log(typeof 'hello');
+console.log(typeof "Hello its raining.");
+
+
+
+
+let word="raining";
+console.log(word.toUpperCase());
+
+//ERROR
+/*let number=10;
+console.log(number.toUpperCase());*/
+
+
+
+
+console.log('hello and good evening'.length);
+console.log("hello".charAt(4));
+
+
+
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof 'five'.isNaN);
+console.log(isNaN('five'));
+
+
+
+
+if(0){
+    console.log("Very true!")
+}else{
+    console.log("Very wrong!")
+}
+
+
+
+console.log('hello'.includes('e'));
+
+
+
+
+let score2;
+let result2=null;
+
+if(result2){
+    console.log("Very true!")
+}else{
+    console.log("Very wrong!")
+}
+
+console.log(typeof score2)
+console.log(typeof result2)
